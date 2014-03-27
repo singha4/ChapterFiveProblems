995cc5e489c45c450cdbc9b71d58891f11d70a63
singha4 <amandeep159632478@gmail.com>
2014-03-27 10:03:41 -0400

    version 1

---
+++ ChapterFiveProblems/IntegerList/IntegerList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IntegerList
+{
+    class IntegerList
+    {
+        static void Main(string[] args)
+        {
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                grades[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(grades);
+                
+            }
+        }
+    }
+}
